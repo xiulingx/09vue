4044c414cf1b307852d777a5d291fc1375fba266
kase <2273564693@qq.com>
2019-12-14 08:33:37 +0800

    12.14

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import 'lib-flexible'
+
+import App from './App.vue'
+import router from './router'
+import Header from './components/Header/Header.vue'
+
+// 注册全局组件
+Vue.component('Header', Header)
+
+new Vue({
+  // components: {
+  //   App
+  // },
+  // template: '<App/>'
+  render: h => h(App),
+  router, // 所有组件都能看到 $router和$route  <router-link> 和 <router-view/>
+}).$mount('#app')
